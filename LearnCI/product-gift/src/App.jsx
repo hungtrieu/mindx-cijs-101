e594731036f4681755039a26cae1c4d2e5d801d2
Hung Trieu <hungtrieu2910@gmail.com>
2024-11-22 10:20:50 +0700

    Lesson 06 practice

--- LearnCI/product-gift/src/App.jsx
+++ LearnCI/product-gift/src/App.jsx
@@ -4,6 +4,7 @@ import Filter from './components/Filter'
 import GiftItem from './components/GiftItem'
 import Pagination from './components/Pagination'
 import Modal from './components/Modal'
+import ModalCreate from './components/ModalCreate'
 import giftsList from './data';
 
 import './App.css'
@@ -11,10 +12,30 @@ import './App.css'
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
+  const [openModalCreate, setOpenModalCreate] = useState(false);
+  const [selectedGift, setSelectedGift] = useState();
 
-  const handleOpenModal = (open) => {
+  const handleOpenModal = (open, gift) => {
     setOpenModal(open);
+    setSelectedGift(gift);
   }
+
+  const handleOpenModalCreate = (open) => {
+    setOpenModalCreate(open);
+    setNewGift({
+      name: '',
+      price: 0,
+      imageSrc: '',
+      rating: 0
+    })
+  }
+
+  const [ newGift, setNewGift ] = useState( {
+    name: '',
+    price: 0,
+    imageSrc: '',
+    rating: 0
+  } );
 
   return (
     <div className="container">
@@ -23,16 +44,16 @@ function App() {
       <div className="productGifts">
         <div className="topGift">
           <h2>Quà tặng</h2>
-          <button>Tạo</button>
+          <button onClick={ () => handleOpenModalCreate(true) }>Tạo</button>
         </div>
         <div className="contentGifts">
-          <div className="div1" onClick={ () => handleOpenModal(true) }>
+          <div className="div1" onClick={ () => handleOpenModal(true, giftsList[0]) }>
             <GiftItem item={giftsList[0]} />
             </div>
-          <div className="div2" onClick={ () => handleOpenModal(true) }>
+          <div className="div2" onClick={ () => handleOpenModal(true, giftsList[1]) }>
             <GiftItem item={giftsList[1]} />
             </div>
-          <div className="div3" onClick={ () => handleOpenModal(true) }>
+          <div className="div3" onClick={ () => handleOpenModal(true, giftsList[2]) }>
             <GiftItem item={giftsList[2]} />
             </div>
           <div className="div4"><GiftItem item={giftsList[3]} /></div>
@@ -44,7 +65,8 @@ function App() {
         <Pagination />
       </div>
 
-      { openModal ? <Modal onClose={ () => handleOpenModal(false) } /> : null }
+      { openModal ? <Modal currentGift={selectedGift} onClose={ () => handleOpenModal(false) } /> : null }
+      { openModalCreate ? <ModalCreate newGift={newGift} setNewGift={setNewGift} onClose={ () => handleOpenModalCreate(false) } /> : null }
     </div>
   )
 }
--- LearnCI/product-gift/src/components/Modal/index.jsx
+++ LearnCI/product-gift/src/components/Modal/index.jsx
@@ -1,44 +1,70 @@
-import React from 'react'
+import React, {useState} from 'react'
 import StarYellow from '../../icons/StarYellow';
+import Star from '../../icons/Star';
+import Close from '../../icons/Close';
 
 import './style.css';
 
 const Modal = (props) => {
-    // const closeModal = () => {
-    //     document.getElementById("myModal").style.display = "none";
-    // }
+    const {currentGift} = props;
+    const [comment, setComment ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(currentGift.name, comment);
+    }
 
   return (
     <div id="myModal" className="modal">
-        <div className="modal-content">
-          <span className="close" onClick={props.onClose}>&times;</span>
-          <div className="modalContent">
+        <div className="modalContent">
+            <div className="row control">
+                <Close onClick={props.onClose} />
+            </div>
+          
             <div className="row">
                 <div className="image">
-                    <img src="https://s3-alpha-sig.figma.com/img/4d7f/9b27/bc5102b129c443e15c6c1e76d82104af?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WY2RH~YWiRKIFMeSHLEN1MGKMqbMgLL6C2ArDQEkgSfMNGrCoF2vEiQ6kHkmRfx8EKJBQfNeW6mE1x0xRBamY6Nbz18i~yeXR~YttD-2kOmbAZjXh3RhXp-3HSajPdfoqu~sFRDXAAnZ9VO9Ug4X2jsdC7C7wRgmUmEsnBj79gM7S5WDzdAgdP~kj1O4E-k3JT6RA2K-bE-nSuRsO~0BCiXRzQKuGAHOYDYoXUf45xNvKEXuIUonGL4vpvThIIj4srZHKF3yGffw8VgMnk2vAAHYDeDlYSdN2AypKyuPBi-MAhXHP717tYCWRtVhh3v2GSwRouH9xVemG-A5AS2ShQ__" />
+                    <img src={currentGift.imageSrc} />
                 </div>
                 <div className="information">
-                    <h2>(product name)</h2>
-                    <div className="rating">
+                    <h2 className="name irow">{currentGift.name}</h2>
+                    <div className="rating irow">
                         <StarYellow />
                         <StarYellow />
                         <StarYellow />
+                        <Star />
+                        <Star />
                     </div>
-                    <div className="price">
-                        (price)
+                    <div className="price irow">
+                        Giá: 42.000 VND
                     </div>
-                    <div className="classified">
-                        (classified)
+                    <div className="classified irow">
+                        Phân loại:
+                        <div className="buttons">
+                            <button>Nhỏ</button>
+                            <button>Vừa</button>
+                            <button>Lớn</button>
+                        </div>
                     </div>
                     <div className="addToCart">
-                        (add to cart)
+                        <div className="amount">
+                            <button>-</button>
+                            <input type="text" defaultValue={0} />
+                            <button>+</button>
+                        </div>
+                        <button className="buy">Chọn mua</button>
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="description row">
                 (description)
             </div>
-          </div>
+            <div className="comment row">
+                <form onSubmit={(event) => handleSubmit(event)}>
+                    <textarea value={comment} onChange={ (e) => setComment(e.target.value)}></textarea>
+                    <button>Comment</button>
+                </form>
+                
+            </div>
         </div>
     </div>
   )
